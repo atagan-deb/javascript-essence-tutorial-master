f83b7322899ad003a94e0d9d7f075fbeb9e606de
atagan-deb <yusei.deb@gmail.com>
2022-01-11 06:59:35 +0900

    new演算子まで受講

--- 060_関数とオブジェクト２/020_prototype/start/main.js
+++ 060_関数とオブジェクト２/020_prototype/start/main.js
@@ -1,8 +1,20 @@
 function Person(name, age) {
-    this.name = name;
-    this.age = age;
+  this.name = name;
+  this.age = age;
+  this.hi = function () {
+    console.log(`hi, ${this.name}`);
+  };
 }
 
-const bob = new Person('Bob', 18);
-const tom = new Person('Tom', 33);
-const sun = new Person('Sun', 20);
+// プロトタイプ
+Person.prototype.hello = function () {
+  console.log(`hello, ${this.name}`);
+};
+
+const bob = new Person("Bob", 18);
+const tom = new Person("Tom", 33);
+const sun = new Person("Sun", 20);
+
+bob.hello();
+tom.hello();
+tom.hi();
