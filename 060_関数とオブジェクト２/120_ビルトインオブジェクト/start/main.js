0a539e7b666525e1ee28a33f70b73571a67cf864
atagan-deb <yusei.deb@gmail.com>
2022-01-14 07:04:10 +0900

    getter, setter, staticまで受講

---
+++ 060_関数とオブジェクト２/120_ビルトインオブジェクト/start/main.js
@@ -0,0 +1,8 @@
+const arr0 = new window.Array(1, 2, 3, 4);
+console.log(arr0);
+const arr = new Array(1, 2, 3, 4);
+console.log(arr);
+
+console.log(arr[0]);
+console.log(arr["0"]);
+console.log(arr.hasOwnProperty(0));
--- 060_関数とオブジェクト２/160_getter、setterとstatic/start/main.js
+++ 060_関数とオブジェクト２/160_getter、setterとstatic/start/main.js
@@ -1,4 +1,42 @@
 function Person1(name, age) {
-	this.name = name;
-	this.age = age;
+  this.__name = name;
+  this.age = age;
 }
+
+Object.defineProperty(Person1.prototype, "name", {
+  get: function () {
+    return this._name;
+  },
+  set: function (val) {
+    this._name = val;
+  },
+});
+
+const p1 = new Person1("Bob", 23);
+console.log(p1.name);
+p1.name = "Tom";
+console.log(p1.name);
+console.log(p1);
+
+class Person2 {
+  constructor(name, age) {
+    this.__name = name;
+    this.age = age;
+  }
+  get name() {
+    return this._name;
+  }
+  set name(val) {
+    this._name = val;
+  }
+	static hello(){ // 静的メソッド
+		console.log('helllo' + this);
+	}
+}
+
+const p2 = new Person2("Bob", 23);
+console.log(p2.name);
+p2.name = "Tom";
+console.log(p2.name);
+console.log(p2);
+Person2.hello(); // 静的メソッドはコンストラクタから直接呼び出せる
