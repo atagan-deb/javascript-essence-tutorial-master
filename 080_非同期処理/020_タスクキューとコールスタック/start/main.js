f824b70a56226af01db92e1b200997700a54a77d
atagan-deb <yusei.deb@gmail.com>
2022-01-17 07:02:39 +0900

    コールバック関数と非同期処理まで受講

--- 080_非同期処理/020_タスクキューとコールスタック/start/main.js
+++ 080_非同期処理/020_タスクキューとコールスタック/start/main.js
@@ -1,15 +1,24 @@
-const btn = document.querySelector('button');
-btn.addEventListener('click', function task2() {
-    console.log('task2 done');
+// タスクキュー：非同期処理の実行順を管理
+// FIFO
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", function task2() {
+  console.log("task2 done");
 });
 
 function a() {
-    setTimeout(function task1() {
-        console.log('task1 done');
-    }, 4000);
+  setTimeout(function task1() {
+    console.log("task1 done");
+  }, 4000);
 
-    const startTime = new Date();
-    while (new Date() - startTime < 5000);
+  const startTime = new Date();
+  while (new Date() - startTime < 5000);
 
-    console.log('fn a done');
+  console.log("fn a done");
 }
+
+a();
+
+// コールスタックにコンテキストが積まれている間はタスクキューからイベントループを通じて
+// コールスタックにタスクを渡すことができない
+// コールスタックでは順次タスクが消化されていく
