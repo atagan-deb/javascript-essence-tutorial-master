945ca82d4b26963feac857b80ace49dda18ac931
atagan-deb <yusei.deb@gmail.com>
2022-01-18 07:00:40 +0900

    100_AwaitとAsyncまで受講

--- 080_非同期処理/060_Promiseチェーン/start/main.js
+++ 080_非同期処理/060_Promiseチェーン/start/main.js
@@ -1,6 +1,28 @@
-function sleep(callback, val) {
-  setTimeout(function() {
-    console.log(val++);
-    callback(val);
-  }, 1000);
+// function sleep(callback, val) {
+function sleep(val) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      console.log(val++);
+      resolve(val);
+      // callback(val);
+    }, 1000);
+  });
 }
+
+sleep(0)
+  .then(function (val) {
+    return sleep(val);
+  })
+  .then(function (val) {
+    return sleep(val);
+  })
+  .then(function (val) {
+    sleep(val);
+    return val; // promiseが返却されない
+  })
+  .then(function (val) {
+    return sleep(val);
+  })
+  .then(function (val) {
+    return sleep(val);
+  });
