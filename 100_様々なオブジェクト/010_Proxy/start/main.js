f42380d8e8273c975e55bb948ac2d88a3a041973
atagan-deb <yusei.deb@gmail.com>
2022-01-22 08:33:53 +0900

    050_WeakMapとプライベート変数まで受講

--- 100_様々なオブジェクト/010_Proxy/start/main.js
+++ 100_様々なオブジェクト/010_Proxy/start/main.js
@@ -1,1 +1,25 @@
 const targetObj = { a: 0 };
+
+const handler = {
+  set: (target, prop, value, receiver) => {
+    console.log(receiver);
+    console.log(`[set]: ${prop}`);
+    // target[prop] = value;
+    throw new Error("cannot add prop.");
+  },
+  get: (target, prop, receiver) => {
+    console.log(receiver);
+    console.log(`[get]: ${prop}`);
+    return target[prop];
+  },
+  deleteProperty: (target, prop) => {
+    console.log(`[delete]: ${prop}`);
+    delete target[prop];
+  },
+};
+
+const pxy = new Proxy(targetObj, handler);
+// pxy.a = 1;
+pxy.a;
+delete pxy.a;
+console.log(pxy);
--- 100_様々なオブジェクト/030_ReflectとProxy/start/main.js
+++ 100_様々なオブジェクト/030_ReflectとProxy/start/main.js
@@ -1,13 +1,45 @@
-const targetObj = { a: 0 };
+const targetObj = {
+  a: 0,
+  get value() {
+    return this.b;
+  },
+};
 
+// const handler = {
+//   get: function(target, prop, receiver) {
+//     console.log(`[get]: ${prop}`);
+//     if(target.hasOwnProperty(prop)) {
+//       return target[prop];
+//     } else {
+//       return -1;
+//     }
+//   }
+// }
+// const handler = {
+//   get: function (target, prop, receiver) {
+//     console.log(`[get]: ${prop}`);
+//     if (target.hasOwnProperty(prop)) {
+//       return Reflect.get(target, prop);
+//     } else {
+//       return -1;
+//     }
+//   },
+// };
 const handler = {
-  get: function(target, prop, receiver) {
+  get: function (target, prop, receiver) {
     console.log(`[get]: ${prop}`);
-    if(target.hasOwnProperty(prop)) {
-      return target[prop];
+    // console.log(receiver);
+    if (target.hasOwnProperty(prop)) {
+      return Reflect.get(target, prop, receiver);
     } else {
       return -1;
     }
-  }
-}
+  },
+};
 const pxy = new Proxy(targetObj, handler);
+// console.log(pxy.a);
+// console.log(pxy.b);
+
+console.log(pxy.value);
+// console.log(targetObj.b);
+console.log(pxy.b);
