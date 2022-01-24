0b2fba518bb36ecd37e6a8ed09955c99cb7ec958
atagan-deb <yusei.deb@gmail.com>
2022-01-25 07:04:19 +0900

    030_Reactivity (Proxy編)まで受講
    proxyとreflectの使い方になれないなぁ

--- 110_Vue/030_Reactivity（Proxy編）/start/reactive.js
+++ 110_Vue/030_Reactivity（Proxy編）/start/reactive.js
@@ -1,3 +1,25 @@
+const handler = {
+  set: (target, key, value, receiver) => {
+    const res = Reflect.set(target, key, value, receiver);
+    console.log(
+      "%c[reactive:set]",
+      "background: green; color: white;",
+      key,
+      res
+    );
+    trigger();
+    return res;
+  },
+  get: (target, key, receiver) => {
+    const res = Reflect.get(target, key, receiver);
+    console.log("%c[reactive:get]", "background: blue; color: white", key, res);
+    return res;
+  },
+};
+function reactive(target) {
+  return new Proxy(target, handler);
+}
+
 let activeEffect = null;
 function effect(fn) {
   activeEffect = fn;
@@ -5,4 +27,5 @@ function effect(fn) {
 function trigger() {
   activeEffect();
 }
-export { effect, trigger };
+
+export { effect, reactive };
